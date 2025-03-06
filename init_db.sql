ae63bd84d14218fd6de8e5d1a60dc7727a16b82a
legan80 <oleg.grch@gmail.com>
2025-03-06 23:55:51 +0300

    Initial commit

--- init_db.sql
+++ init_db.sql
@@ -1,24 +1,32 @@
 -- Создание таблицы users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT PRIMARY KEY,
     name TEXT,
     username TEXT,
     phone TEXT,
     birth_date DATE,
     is_subscriber BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()  -- Указываем тип timestamp with time zone
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
 -- Создание таблицы requests
-CREATE TABLE requests (
+CREATE TABLE IF NOT EXISTS requests (
     request_id SERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(user_id),
     message_text TEXT,
     parsed_title TEXT,
     parsed_category TEXT,
     generated_text TEXT,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()  -- Указываем тип timestamp with time zone
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+-- Создание пользователя avitobot (если его нет)
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'avitobot') THEN
+        CREATE USER avitobot WITH PASSWORD 'ваш_пароль';
+    END IF;
+END $$;
 
 -- Предоставление прав пользователю avitobot
 GRANT ALL PRIVILEGES ON TABLE users TO avitobot;
